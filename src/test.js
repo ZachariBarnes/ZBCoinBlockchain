37ce5d47eb05e0322300f7ff8e214b3436a2268a
Zachari Barnes <ZachariBarnes@yahoo.com>
2021-10-19 20:25:49 -0500

    Uncommenting Tests, need to Jestify them still

--- src/test.js
+++ src/test.js
@@ -7,22 +7,22 @@ const bitcoin = new Blockchain();
 bitcoin.CreateNewBlock(2389, 'OIN89SDN90N', '90IOSDF45SDFKE');
 
 // Test Adding multiple items to the block chain
-// bitcoin.CreateNewBlock(111, 'OIN89SD6345N', '234SDF34SFGGR0');
-// bitcoin.CreateNewBlock(23189, 'OIN12ASDN90N', '90IOSDF432DFKE');
+bitcoin.CreateNewBlock(111, 'OIN89SD6345N', '234SDF34SFGGR0');
+bitcoin.CreateNewBlock(23189, 'OIN12ASDN90N', '90IOSDF432DFKE');
 
 // Test Adding a malicous block to the chain
 
 // Test creating a transaction
-// bitcoin.CreateNewTransaction(100, 'ALEXFWAE543SDF464', 'JENSDF546S5D4F534SF');
-// bitcoin.CreateNewBlock(32423452, 'ADSDGFE435S67', 'SDFERWF432D24');
-// console.log(bitcoin.chain[bitcoin.chain.length]);
+bitcoin.CreateNewTransaction(100, 'ALEXFWAE543SDF464', 'JENSDF546S5D4F534SF');
+bitcoin.CreateNewBlock(32423452, 'ADSDGFE435S67', 'SDFERWF432D24');
+console.log(bitcoin.chain[bitcoin.chain.length]);
 
-// // Test creating multiple transations
-// bitcoin.CreateNewTransaction(50, 'ALEXFWAE543SDF464', 'JENSDF546S5D4F534SF');
-// bitcoin.CreateNewTransaction(300, 'ALEXFWAE543SDF464', 'JENSDF546S5D4F534SF');
-// bitcoin.CreateNewTransaction(2000, 'ALEXFWAE543SDF464', 'JENSDF546S5D4F534SF');
-// bitcoin.CreateNewBlock(2342345, 'AD23GFE435S67', 'SDF5WF432D24');
-// console.log(bitcoin.chain[bitcoin.chain.length]);
+// Test creating multiple transations
+bitcoin.CreateNewTransaction(50, 'ALEXFWAE543SDF464', 'JENSDF546S5D4F534SF');
+bitcoin.CreateNewTransaction(300, 'ALEXFWAE543SDF464', 'JENSDF546S5D4F534SF');
+bitcoin.CreateNewTransaction(2000, 'ALEXFWAE543SDF464', 'JENSDF546S5D4F534SF');
+bitcoin.CreateNewBlock(2342345, 'AD23GFE435S67', 'SDF5WF432D24');
+console.log(bitcoin.chain[bitcoin.chain.length]);
 
 // Test HashBlock Method
 const prevHash = 'SA65F465S4DF6SA54FASDF';
@@ -35,4 +35,4 @@ const nonce = 100;
 
 console.log(bitcoin.HashBlock(prevHash, currentBlock, nonce));
 
-// console.log(bitcoin);
+console.log(bitcoin);
