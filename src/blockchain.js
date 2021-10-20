293d195ae1cf99e36f7de7ca7cca174205146c99
Zachari Barnes <Zachari.Barnes@yahoo.com>
2021-10-19 20:21:20 -0500

    Adding hashBlock function, Enforcing Eslint now

--- src/blockchain.js
+++ src/blockchain.js
@@ -1,42 +1,47 @@
-// const sha256 = require('sha256');
+const sha256 = require('sha256');
 // const rewe;
 
 class Blockchain {
-    constructor() {
-        this.chain = [];
-        this.pendingTransactions = [];
-    }
+  constructor() {
+    this.chain = [];
+    this.pendingTransactions = [];
+  }
 
-    CreateNewBlock(nonce, preciousBlockHash, hash) {
-        const newBlock = {
-            index: this.chain.length + 1,
-            timestap: Date.now(),
-            transactions: this.pendingTransactions,
-            nonce, //Proof that this block was created legitmately
-            hash,
-            preciousBlockHash
-        };
-    
-        this.pendingTransactions = [];
-        this.chain.push(newBlock);
-        return newBlock;
-    }
+  CreateNewBlock(nonce, preciousBlockHash, hash) {
+    const newBlock = {
+      index: this.chain.length + 1,
+      timestap: Date.now(),
+      transactions: this.pendingTransactions,
+      nonce, // Proof that this block was created legitmately
+      hash,
+      preciousBlockHash,
+    };
 
-    GetLastBlock() {
-        return this.chain[this.chain.length - 1];
-    }
+    this.pendingTransactions = [];
+    this.chain.push(newBlock);
+    return newBlock;
+  }
 
-    //Notes: Supporting multiple token types requires tokenId
-    //Possible override usecase: To support sending multiple token types or two way exchanges at once requires this to accept an array.
-    CreateNewTransaction(amount, /*tokenId,*/ sender, recipient) {
-        const newTransaction = {
-            amount,
-            sender,
-            recipient
-        };
-        this.pendingTransactions.push(newTransaction);
-        return this.GetLastBlock()[index]+1; //Return the index of the block that will confirm this transaction.
-    }
+  GetLastBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  // Notes: Supporting multiple token types requires tokenId
+  // Possible override usecase: To support sending multiple token types or two way exchanges at once requires this to accept an array.
+  CreateNewTransaction(amount, /* tokenId, */ sender, recipient) {
+    const newTransaction = {
+      amount,
+      sender,
+      recipient,
+    };
+    this.pendingTransactions.push(newTransaction);
+    return this.GetLastBlock()[this.index] + 1; // Return the index of the block that will confirm this transaction.
+  }
+
+  HashBlock(previousBlockHash, currentBlock, nonce) {
+    const hash = sha256(`${previousBlockHash}${nonce.toString()}${JSON.stringify(currentBlock)}`);
+    return hash;
+  }
 }
 
 module.exports = Blockchain;
--- src/test.js
+++ src/test.js
@@ -1,20 +1,38 @@
-const Blockchain = require( "./blockchain");
+const Blockchain = require('./blockchain');
 
-
-//Test Creating a blockchain
+// Test Creating a blockchain
 const bitcoin = new Blockchain();
 
-//Test Adding Items to the block chain
+// Test Adding Items to the block chain
 bitcoin.CreateNewBlock(2389, 'OIN89SDN90N', '90IOSDF45SDFKE');
 
-//Test Adding multiple items to the block chain
-bitcoin.CreateNewBlock(111, 'OIN89SD6345N', '234SDF34SFGGR0');
-bitcoin.CreateNewBlock(23189, 'OIN12ASDN90N', '90IOSDF432DFKE');
+// Test Adding multiple items to the block chain
+// bitcoin.CreateNewBlock(111, 'OIN89SD6345N', '234SDF34SFGGR0');
+// bitcoin.CreateNewBlock(23189, 'OIN12ASDN90N', '90IOSDF432DFKE');
 
-//Test Adding a malicous block to the chain
+// Test Adding a malicous block to the chain
 
-//Test creating a transaction
+// Test creating a transaction
+// bitcoin.CreateNewTransaction(100, 'ALEXFWAE543SDF464', 'JENSDF546S5D4F534SF');
+// bitcoin.CreateNewBlock(32423452, 'ADSDGFE435S67', 'SDFERWF432D24');
+// console.log(bitcoin.chain[bitcoin.chain.length]);
 
+// // Test creating multiple transations
+// bitcoin.CreateNewTransaction(50, 'ALEXFWAE543SDF464', 'JENSDF546S5D4F534SF');
+// bitcoin.CreateNewTransaction(300, 'ALEXFWAE543SDF464', 'JENSDF546S5D4F534SF');
+// bitcoin.CreateNewTransaction(2000, 'ALEXFWAE543SDF464', 'JENSDF546S5D4F534SF');
+// bitcoin.CreateNewBlock(2342345, 'AD23GFE435S67', 'SDF5WF432D24');
+// console.log(bitcoin.chain[bitcoin.chain.length]);
 
+// Test HashBlock Method
+const prevHash = 'SA65F465S4DF6SA54FASDF';
+const currentBlock = [
+  bitcoin.CreateNewTransaction(2000, 'ALEXFWAE543SDF464', 'JENSDF546S5D4F534SF'),
+  bitcoin.CreateNewTransaction(300, 'ALEXFWAE543SDF464', 'JENSDF546S5D45534SF'),
+  bitcoin.CreateNewTransaction(100, 'ALEXFWAE543SDF464', 'JENSDF546S5D8S534SF'),
+];
+const nonce = 100;
 
-console.log(bitcoin);
+console.log(bitcoin.HashBlock(prevHash, currentBlock, nonce));
+
+// console.log(bitcoin);
