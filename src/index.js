f06b6bad53bb7cab76fca69c41694b4d9bdc6808
Zachari Barnes <ZachariBarnes@yahoo.com>
2021-10-25 10:56:44 -0500

    emptying working file

--- src/index.js
+++ src/index.js
@@ -1,17 +1,4 @@
 const Blockchain = require('./blockchain');
 
 const testCoin = new Blockchain();
-testCoin.CreateNewTransaction(2000, 'ALEXFWAE543SDF464', 'JENSDF546S5D4F534SF');
-const block = {
-  index: 1,
-  timestamp: 15234545,
-  transactions: [{
-    amount: 100,
-    sender: 'ALEXFWAE543SDF464',
-    recipient: 'JENSDF546S5D4F534SF',
-  }],
-  nonce: 15234545,
-  previousHash: '2919b034e036a991230a678075cd04993b800897186a7d574d944a7b8ef8296d',
-};
-const nonce = testCoin.ProofOfWork('2919b034e036a991230a678075cd04993b800897186a7d574d944a7b8ef8296d', block);
-console.log(nonce);
+console.log(testCoin);
