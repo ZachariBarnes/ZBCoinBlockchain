942b27a2e63626a412407756d6401eeae86b243d
Zachari Barnes <ZachariBarnes@yahoo.com>
2021-10-28 20:00:25 -0500

    mine endpoint

--- src/api.js
+++ src/api.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
+const uuid = require('uuid/v4');
 const express = require('express');
 const logger = require('npmlog');
 const Blockchain = require('./blockchain');
 
+const nodeAddress = uuid().split('-').join('');
 const ztblockChain = new Blockchain();
 const port = process.env.PORT || 2500;
 const app = express();
@@ -20,7 +22,15 @@ app.post('/transaction/new', (req, res) => {
 });
 
 app.get('/mine', (req, res) => {
-
+  const lastBlock = ztblockChain.GetLastBlock();
+  const previousBlockHash = lastBlock.hash;
+  const currentBlockData = { transactions: ztblockChain.pendingTransactions, index: lastBlock.index + 1 };
+  const nonce = ztblockChain.ProofOfWork(previousBlockHash, currentBlockData);
+  const blockHash = ztblockChain.HashBlock(previousBlockHash, currentBlockData, nonce);
+  // Reward the miner
+  ztblockChain.CreateNewTransaction(12.5, '00', '00'/* recipient Address */);
+  const newBlock = ztblockChain.CreateNewBlock(nonce, previousBlockHash, blockHash);
+  res.json({ note: 'New Block mined successfully.', block: newBlock.index });
 });
 
 app.listen(port, () => {
