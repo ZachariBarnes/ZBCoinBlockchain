0c9cb1da0f54dddaefe0653505f7e4b5b8d2fd4f
Zachari Barnes <ZachariBarnes@yahoo.com>
2021-10-28 20:06:53 -0500

    rename coin

--- src/api.js
+++ src/api.js
@@ -5,31 +5,31 @@ const logger = require('npmlog');
 const Blockchain = require('./blockchain');
 
 const nodeAddress = uuid.v4().split('-').join('');
-const ztblockChain = new Blockchain();
+const zbCoin = new Blockchain();
 const port = process.env.PORT || 2500;
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get('/blockchain', (req, res) => {
-  res.send(ztblockChain);
+  res.send(zbCoin);
 });
 
 app.post('/transaction/new', (req, res) => {
   const { amount, sender, recipient } = req.body;
-  const blockIndex = ztblockChain.CreateNewTransaction(amount, sender, recipient);
+  const blockIndex = zbCoin.CreateNewTransaction(amount, sender, recipient);
   res.json({ note: `New Transaction created. It will be added in block: ${blockIndex}` });
 });
 
 app.get('/mine', (req, res) => {
-  const lastBlock = ztblockChain.GetLastBlock();
+  const lastBlock = zbCoin.GetLastBlock();
   const previousBlockHash = lastBlock.hash;
-  const currentBlockData = { transactions: ztblockChain.pendingTransactions, index: lastBlock.index + 1 };
-  const nonce = ztblockChain.ProofOfWork(previousBlockHash, currentBlockData);
-  const blockHash = ztblockChain.HashBlock(previousBlockHash, currentBlockData, nonce);
+  const currentBlockData = { transactions: zbCoin.pendingTransactions, index: lastBlock.index + 1 };
+  const nonce = zbCoin.ProofOfWork(previousBlockHash, currentBlockData);
+  const blockHash = zbCoin.HashBlock(previousBlockHash, currentBlockData, nonce);
   // Reward the miner
-  ztblockChain.CreateNewTransaction(12.5, '00', nodeAddress);
-  const newBlock = ztblockChain.CreateNewBlock(nonce, previousBlockHash, blockHash);
+  zbCoin.CreateNewTransaction(12.5, '00', nodeAddress);
+  const newBlock = zbCoin.CreateNewBlock(nonce, previousBlockHash, blockHash);
   res.json({ note: 'New Block mined successfully.', block: newBlock.index });
 });
 
